bad990c570c26c7a5f0333816bbddc8c491c28f6
Laukpindang <ferdiansyahbandung3@gmail.com>
2024-09-27 16:27:13 +0700

    double

--- tiga.java
+++ tiga.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 class Tiga {
   public static void main(String[] args) {
     // Variable
-    int jariJari;
+    double jariJari;
     double pi = 3.14;
     Scanner scan = new Scanner(System.in);
 
     // Masukkan jari-jari
     System.out.print("Masukkan jari-jari: ");
-    jariJari = scan.nextInt();
+    jariJari = scan.nextDouble();
 
     // tampilkan luas lingkaran (pi kali r kali r)
     System.out.print("Luas lingkaran: " + (pi * jariJari * jariJari));
